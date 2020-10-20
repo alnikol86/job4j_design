bc138ffad67bff6a38e4729d337eb844b0a75190
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-10-20 23:53:55 +0300

    iter draft3

--- chapter_001/src/main/java/ru/job4j/it/MatrixIt.java
+++ chapter_001/src/main/java/ru/job4j/it/MatrixIt.java
@@ -14,7 +14,16 @@ public class MatrixIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        return row < data.length && column < data.length;
+        if (row >= data.length) {
+            return false;
+        }
+        if (column >= data[row].length && row == data.length - 1) {
+            return false;
+        }
+        if (data[row][column] null) {
+            next();
+        }
+        return true;
     }
 
     @Override
@@ -22,6 +31,10 @@ public class MatrixIt implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        return data[row++][column++];
+        if (column >= data[row].length) {
+            row++;
+            column = 0;
+        }
+        return data[row][column++];
     }
 }
