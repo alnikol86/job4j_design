0993cd8672b9349cc59e7130bcb406ec6bfa0c00
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-10-30 23:16:48 +0300

    draft1_FlatMap

---
+++ chapter_001/src/test/java/ru/job4j/iterator/FlatMapTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class FlatMapTest {
+    @Test
+    public void whenDiffNext() {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1).iterator(),
+                List.of(2, 3).iterator()
+        ).iterator();
+        FlatMap<Integer> flat = new FlatMap<>(data);
+        assertThat(flat.next(), is(1));
+        assertThat(flat.next(), is(2));
+        assertThat(flat.next(), is(3));
+    }
+}
