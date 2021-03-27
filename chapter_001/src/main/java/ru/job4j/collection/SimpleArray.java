f0cd6404fad497ccb6a45c4a92acfdb2f6eb6284
Alexander Nikolaev <nikolaevairman@gmail.com>
2021-03-28 00:08:03 +0300

    1. Динамический список на массиве.[158#386023]

---
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleArray.java
@@ -0,0 +1,61 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private T[] containers;
+    private int capasity = 1;
+    private int size;
+    private int modCount;
+
+    public SimpleArray() {
+        this.containers = (T[]) new Object[capasity];
+    }
+
+    public SimpleArray(int manualCapasity) {
+        this.containers = (T[]) new Object[manualCapasity];
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return containers[index];
+    }
+
+    public void add(T model) {
+        modCount++;
+        if (containers.length <= size) {
+            grow();
+        }
+        containers[size++] = model;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int point = 0;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!(hasNext())) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return containers[point++];
+            }
+        };
+    }
+    private void grow() {
+        capasity = capasity * 2;
+        if (containers.length <= size) {
+            containers = Arrays.copyOf(containers, capasity);
+        }
+    }
+}
