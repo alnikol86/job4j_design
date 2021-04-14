98aa2666d0b0ba349625da9309fcd67d2f7c327a
Alexander Nikolaev <nikolaevairman@gmail.com>
2021-04-14 10:49:39 +0300

    5. Очередь на двух стеках[160#386025][2]

--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,7 +5,7 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> out = new SimpleStack<>();
     private int countIn = 0;
     private int countOut = 0;
-// должен возвращать первое значение и удалять его из коллекции
+
     public T poll() {
         if (countIn != 0) {
             countOut = countIn;
@@ -19,15 +19,14 @@ public class SimpleQueue<T> {
         return rsl;
     }
 
-// помещает значение в конец
     public void push(T value) {
-        if (countOut != 0) {
-            countIn = countOut;
-            for (int i = 0; i < countOut; i++) {
-                in.push(out.poll());
-            }
-            countOut = 0;
-        }
+//        if (countOut != 0) {
+//            countIn = countOut;
+//            for (int i = 0; i < countOut; i++) {
+//                in.push(out.poll());
+//            }
+//            countOut = 0;
+//        }
         in.push(value);
         countIn++;
     }
